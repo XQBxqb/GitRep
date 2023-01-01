796c8cb17f481e213eb8234b68cb81999d876eb4
XQB <2033928942@qq.com>
2023-01-01 21:45:49 +0800

    项目的第一次2

---
+++ src/main/java/com/itheima/spirngboot_02_ssmp/Spirngboot02SsmpApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.spirngboot_02_ssmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spirngboot02SsmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spirngboot02SsmpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itheima/spirngboot_02_ssmp/Spirngboot02SsmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.spirngboot_02_ssmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spirngboot02SsmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
